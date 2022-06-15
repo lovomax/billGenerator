8d4cdc750b01071942d7eda47c36556794a4a6b8
lovomax <riki.arreaza@gmail.com>
2022-06-15 15:03:15 -0400

    intento

--- src/index.js
+++ src/index.js
@@ -3,17 +3,17 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { AppContext } from './context/AppContext';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import Box from 'material-ui/Box'
+
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <AppContext>
-      <Box>
+
         <MuiThemeProvider>
           <App />
         </ MuiThemeProvider>
-      </Box>
+
     </AppContext>
   </React.StrictMode>
 );
