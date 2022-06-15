36da33b873a2e9b7eee6b4b75a6bb782701994cc
lovomax <81450733+lovomax@users.noreply.github.com>
2022-06-15 19:06:00 -0400

    Volví a la rama de archivos anterior

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { AppContext } from './context/AppContext';/*
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';*/
-
+import { AppContext } from './context/AppContext';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <AppContext>
-
-      { /* <MuiThemeProvider>*/}
-          <App />
-      { /* </ MuiThemeProvider>*/}
-
+      <App />
     </AppContext>
   </React.StrictMode>
 );
